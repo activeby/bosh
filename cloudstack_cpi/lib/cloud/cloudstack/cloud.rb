5a4e2f9da88f728041876519d4650bb539129381
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2012-08-01 16:41:20 +0300

    Added basic initialization of CloudStack CPI

--- cloudstack_cpi/lib/cloud/cloudstack/cloud.rb
+++ cloudstack_cpi/lib/cloud/cloudstack/cloud.rb
@@ -1,3 +1,5 @@
+require 'fog'
+
 module Bosh
   module CloudStackCloud
     class Cloud < Bosh::Cloud
@@ -6,6 +8,36 @@ module Bosh
       #
       # @param [Hash] options cloud options
       def initialize(options)
+        @options = options.dup
+
+        validate_options!
+
+        compute_init_options = @options['cloudstack'].select { |key, _|
+          %w(cloudstack_api_key cloudstack_secret_access_key cloudstack_host).include? key
+        }
+        compute_init_options[:provider] = "CloudStack"
+
+        @compute = Fog::Compute.new compute_init_options
+
+
+#options[:cloudstack_session_id] = '?'
+#options[:cloudstack_session_key] = '?'
+#options[:cloudstack_path] = '/client/api'
+#options[:cloudstack_port] = 443
+#options[:cloudstack_scheme] = 'https'
+#options[:cloudstack_persistent] = false
+
+        #vms = compute.list_virtual_machines
+        #p vms
+
+# create volume
+#        compute.volumes.create()
+
+# copy image content to the volume
+#        ``
+
+# create template from the volume
+#        compute.images.create(template_create_params)
       end
 
       include StemcellOperations
@@ -138,6 +170,18 @@ module Bosh
         "`#{method}' is not implemented by #{self.class}"
       end
 
+      def validate_options!
+        unless @options.has_key?('cloudstack') && has_cloudstack_authentication?(@options['cloudstack'])
+          raise ArgumentError, "Invalid CloudStack configuration parameters"
+        end
+      end
+
+      def has_cloudstack_authentication?(cloudstack_config)
+        cloudstack_config.is_a?(Hash) &&
+        cloudstack_config['cloudstack_api_key'] &&
+        cloudstack_config['cloudstack_secret_access_key'] &&
+        cloudstack_config['cloudstack_host']
+      end
     end
   end
 end
---
+++ cloudstack_cpi/spec/unit/cloud_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe Bosh::CloudStackCloud::Cloud do
+  context '#initialize' do
+    let(:correct_parameters) do
+      {
+          'cloudstack' => {
+              'cloudstack_api_key' => 'api_key',
+              'cloudstack_secret_access_key' => 'secret_key',
+              'cloudstack_host' => 'host',
+          }
+      }
+    end
+
+    def new_instance options
+      Bosh::CloudStackCloud::Cloud.new options
+    end
+
+    it 'should receive configuration with cloudstack section' do
+      expect { new_instance({}) }.to raise_error ArgumentError
+    end
+
+    def test_missing_parameters missing_param
+      opt = correct_parameters
+      opt['cloudstack'].delete missing_param
+
+      expect { new_instance({}) }.to raise_error ArgumentError
+    end
+
+    it 'should receive cloudstack_api_key as a cloudstack configuration parameter' do
+      test_missing_parameters 'cloudstack_api_key'
+    end
+
+    it 'should receive cloudstack_secret_access_key as a cloudstack configuration parameter' do
+      test_missing_parameters 'cloudstack_secret_access_key'
+    end
+
+    it 'should receive cloudstack_host as a cloudstack configuration parameter' do
+      test_missing_parameters 'cloudstack_host'
+    end
+
+    it 'should accept correct configuration' do
+      new_instance(correct_parameters)
+    end
+  end
+end
